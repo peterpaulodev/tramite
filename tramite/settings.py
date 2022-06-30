33579dc96b4c74f6d616b6f2fc3a672bb086f91c
Peterson Paulo <peterpaulodev@gmail.com>
2022-06-30 18:41:34 -0300

    chore: commit save point

--- tramite/settings.py
+++ tramite/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'tramite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-if DEBUG:
+if not DEBUG:
     DATABASE_CONFIG = {
         'ENGINE'  : 'django.db.backends.mysql',
         'NAME'    : 'tramite',
@@ -142,7 +142,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/class'
 LOGOUT_REDIRECT_URL = '/accounts/login'
 
-if DEBUG:
+if not DEBUG:
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "static"),
         ('class', 'class/static'),
